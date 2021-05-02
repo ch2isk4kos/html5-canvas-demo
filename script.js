43a7fc518dffb94aba73f110d6307fdc72bae6fb
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-02 00:18:21 -0400

    remove particles when the get to the specified size

--- script.js
+++ script.js
@@ -43,21 +43,22 @@ class Particle {
     // this.y = mouse.y;
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 5 + 1;
+    this.size = Math.random() * 15 + 1;
     this.speedX = Math.random() * 3 - 1.5;
     this.speedY = Math.random() * 3 - 1.5;
   }
   update = () => {
     this.x += this.speedX;
     this.y += this.speedY;
+    if (this.size > 0.2) this.size -= 0.1;
   };
 
   draw = () => {
     ctx.fillStyle = "grey";
     ctx.strokeStyle = "white";
-    ctx.lineWidth = 5;
+    ctx.lineWidth = 2.5;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, 50, 0, Math.PI * 2);
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
     ctx.fill();
     ctx.stroke();
   };
@@ -76,6 +77,11 @@ const handleParticles = () => {
   for (let i = 0; i < particles.length; i++) {
     particles[i].update();
     particles[i].draw();
+
+    if (particles[i].size <= 0.3) {
+      particles.splice(i, 1);
+      i--;
+    }
   }
 };
 
