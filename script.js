89507a32c17eb01cce05ba93fdec079941998af7
ch2isk4kos <ch2isk4kos@gmail.com>
2021-05-02 01:15:17 -0400

    add web animation to particles on hover event

--- script.js
+++ script.js
@@ -77,6 +77,22 @@ const handleParticles = () => {
     particles[i].update();
     particles[i].draw();
 
+    for (let j = i; j < particles.length; j++) {
+      const dx = particles[i].x - particles[j].x;
+      const dy = particles[i].y - particles[j].y;
+
+      //   pythagorean theorem
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      if (distance < 100) {
+        ctx.beginPath();
+        ctx.strokeStyle = particles[i].color;
+        ctx.lineWidth = 0.5;
+        ctx.moveTo(particles[i].x, particles[i].y);
+        ctx.lineTo(particles[j].x, particles[j].y);
+        ctx.stroke();
+      }
+    }
+
     if (particles[i].size <= 0.3) {
       particles.splice(i, 1);
       i--;
@@ -85,13 +101,14 @@ const handleParticles = () => {
 };
 
 const animate = () => {
-  //   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgba(0,0,0,0.02)";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  //   ctx.fillStyle = "rgba(0,0,0,0.02)";
+  //   ctx.fillRect(0, 0, canvas.width, canvas.height);
   handleParticles();
   //   hue++;
   //   hue += 5;
-  hue += 0.5;
+  //   hue += 0.5;
+  hue += 2;
   requestAnimationFrame(animate); // recursive funct.
 };
 
